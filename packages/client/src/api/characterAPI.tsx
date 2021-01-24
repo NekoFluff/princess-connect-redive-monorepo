be921a1e938318f0becc7bf0bf4998ab0a48d766
Alex Nou <alexnou@gmail.com>
2021-01-24 12:07:39 -0700

    Proxy fixed hopefully?

--- packages/client/src/api/characterAPI.tsx
+++ packages/client/src/api/characterAPI.tsx
@@ -1,7 +1,12 @@
 const axios = require("axios").default;
+require("axios-debug-log");
 
 // Make a request for a user with a given ID
 export async function getCharacters() {
+  console.log("AAA2?");
+  require("axios-debug-log");
+  window.localStorage.debug = "axios";
+  // console.log(axios.getUri("/api/characters"));
   const result = await axios.get("/api/characters");
   return result.data;
 }
