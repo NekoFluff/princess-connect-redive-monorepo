14c0fa2a842363d8916814843bb2c975b7e66c4d
Alex Nou <alexnou@gmail.com>
2021-01-24 04:20:15 -0700

    Added /api/ to proxy

--- packages/client/src/api/characterAPI.tsx
+++ packages/client/src/api/characterAPI.tsx
@@ -2,6 +2,6 @@ const axios = require("axios").default;
 
 // Make a request for a user with a given ID
 export async function getCharacters() {
-  const result = await axios.get("/characters");
+  const result = await axios.get("/api/characters");
   return result.data;
 }
