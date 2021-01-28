a4b2c2ff0936f6f28b2a932f5d38c257836ae524
Alex Nou <alexnou@gmail.com>
2021-01-27 22:36:23 -0700

    Add back some functions

--- packages/client/src/components/AreaPage/AreaListContainer.tsx
+++ packages/client/src/components/AreaPage/AreaListContainer.tsx
@@ -13,49 +13,48 @@ type IAreas = { [name: string]: Area };
 const AreaListContainer: React.FC<AreaListContainerProps> = () => {
   const [areas, setAreas] = useState<IAreas>({} as IAreas);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  return <div>Hello</div>;
-  // const handleDeleteArea = async (areaId: string) => {
-  //   const success = await deleteArea(areaId);
-  //   if (success) {
-  //     toast.success(`Successfully deleted area ${areaId}!`);
-  //     const newAreas = await getAreas();
-  //     console.log("New Areas:");
-  //     console.log(newAreas);
-  //     setAreas(newAreas);
-  //   } else {
-  //     toast.error(`Failed to delete area ${areaId}`);
-  //   }
-  // };
+  const handleDeleteArea = async (areaId: string) => {
+    const success = await deleteArea(areaId);
+    if (success) {
+      toast.success(`Successfully deleted area ${areaId}!`);
+      const newAreas = await getAreas();
+      console.log("New Areas:");
+      console.log(newAreas);
+      setAreas(newAreas);
+    } else {
+      toast.error(`Failed to delete area ${areaId}`);
+    }
+  };
 
-  // const handleCreateArea = async (areaName: string) => {
-  //   const newArea = new Area();
-  //   newArea._id = areaName;
-  //   const success = await updateArea(newArea);
+  const handleCreateArea = async (areaName: string) => {
+    const newArea = new Area();
+    newArea._id = areaName;
+    const success = await updateArea(newArea);
 
-  //   if (success) {
-  //     const areas = await getAreas();
-  //     setAreas(areas);
+    if (success) {
+      const areas = await getAreas();
+      setAreas(areas);
 
-  //     toast.success("Successfully created area!");
-  //   } else {
-  //     toast.error("Failed to create area...");
-  //   }
-  // };
+      toast.success("Successfully created area!");
+    } else {
+      toast.error("Failed to create area...");
+    }
+  };
 
-  // useEffect(() => {
-  //   // Create an scoped async function in the hook
-  //   async function anyNameFunction() {
-  //     const areas = await getAreas();
-  //     console.log("Got Areas");
-  //     console.log(areas);
-  //     setAreas(areas);
-  //   } // Execute the created function directly
-  //   anyNameFunction();
+  useEffect(() => {
+    // Create an scoped async function in the hook
+    async function anyNameFunction() {
+      const areas = await getAreas();
+      console.log("Got Areas");
+      console.log(areas);
+      setAreas(areas);
+    } // Execute the created function directly
+    anyNameFunction();
 
-  //   return () => {
-  //     console.log("Cleanup");
-  //   };
-  // }, []);
+    return () => {
+      console.log("Cleanup");
+    };
+  }, []);
 
   // return (
   //   <Container>
@@ -95,6 +94,7 @@ const AreaListContainer: React.FC<AreaListContainerProps> = () => {
   //       })}
   //   </Container>
   // );
+  return <div>Hello</div>;
 };
 
 export default AreaListContainer;
